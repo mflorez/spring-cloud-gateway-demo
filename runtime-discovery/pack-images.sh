fb751230294d94055a567794547de2c50747a1f7
Moises Florez <moises.florez@cyone.com>
2021-01-30 14:48:59 -0500

    To fix registry service not staring, replaced "BP_JAVA_VERSION=8.*" with "BP_JVM_VERSION=8.*".
    Post: https://spring.io/blog/2019/07/01/hiding-services-runtime-discovery-with-spring-cloud-gateway
    Dear Ben & Brian,
    Thanks for your great article.
    
    The registry service had a problem to start tomcat because of the absence of javax.xml.bind.jaxbcontext.
    Although in pack-images.sh it is mentioned to use java8 like --env "BP_JAVA_VERSION=8.*" , it was still using JVM version 11.
    After spending couple of hours I replaced "BP_JAVA_VERSION=8.*" with BP_JVM_VERSION=8.* and now it works.
    
    I hope it can help anyone who faces this issue.
    
    Best regards,
    Pooya

--- runtime-discovery/pack-images.sh
+++ runtime-discovery/pack-images.sh
@@ -8,15 +8,15 @@ pack set-default-builder cloudfoundry/cnb:bionic
 
 echo "Packing the Service"
 cd service
-pack build scg-demo-service --env "BP_JAVA_VERSION=8.*"
+pack build scg-demo-service --env "BP_JVM_VERSION=8.*"
 cd ..
 
 echo "Packing the Eureka Discovery Server"
 cd registry
-pack build scg-demo-registry --env "BP_JAVA_VERSION=8.*"
+pack build scg-demo-registry --env "BP_JVM_VERSION=8.*"
 cd ..
 
 echo "Packing the Spring Cloud Gateway"
 cd gateway
-pack build scg-demo-gateway --env "BP_JAVA_VERSION=8.*"
+pack build scg-demo-gateway --env "BP_JVM_VERSION=8.*"
 cd ..
